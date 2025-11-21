425d94e5e3466490982b8773d074a357e37ba539
Guilherme Felix <guilherme.flima@ufape.edu.br>
2025-11-21 11:45:51 -0300

    refactor(src): code comments removed

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import keycloak from './keycloak';
 
-// 1. Cria a instância do axios
 const api = axios.create({
-  baseURL: 'http://localhost:8080' // Sua API Spring Boot
+  baseURL: 'http://localhost:8080'
 });
 
-// 2. Interceptor (o código é o mesmo)
 api.interceptors.request.use(
   (config) => {
     if (keycloak.authenticated && keycloak.token) {
