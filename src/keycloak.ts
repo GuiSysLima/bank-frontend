40d29b72bcea33b298abc7853a719f793385d789
Guilherme Felix <guilherme.flima@ufape.edu.br>
2026-01-03 10:48:56 -0300

    refactor: deploy configuration

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,10 +1,9 @@
 import Keycloak from 'keycloak-js';
 
-// A configuração é a mesma
 const keycloak = new Keycloak({
-  url: 'http://localhost:8180',
+  url: import.meta.env.VITE_KEYCLOAK_URL || "http://localhost:8180",
   realm: 'bank-realm',
-  clientId: 'bank-frontend' // O Client ID que você criou para o React
+  clientId: 'bank-frontend'
 });
 
 export default keycloak;
